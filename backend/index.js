1ab9ef4aad194b088c207eda994758a75e1f9665
farhan ansari <farhanraza2239@gmail.com>
2024-03-12 00:11:22 +0530

    backend setup

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+
+import dotenv from 'dotenv'
+dotenv.config()
+
+import express from 'express'
+
+const app=express()
+const PORT=process.env.PORT || 3000
+
+
+app.listen(PORT,()=>{
+    console.log(`server running on ${PORT}`)
+})
