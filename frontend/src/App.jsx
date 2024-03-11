9db62ac9cbdb7266718cedf58afb5b91a8898cad
farhan ansari <farhanraza2239@gmail.com>
2024-03-11 23:11:22 +0530

    create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
 
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
+import Projects from './pages/Projects'
 
 function App() {
   
 
   return (
-    <>
-      <h1 className=' text-red-700'>react app</h1>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
