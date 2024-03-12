548b2c63fd82e3affc390d12d830e4ffa329af6b
farhan ansari <farhanraza2239@gmail.com>
2024-03-12 17:12:44 +0530

    add login functionality

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -116,8 +116,8 @@ function Signup() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
-            <Link to='/sign-in' className='text-blue-500'>
-              Sign In
+            <Link to='/login' className='text-blue-500'>
+              login
             </Link>
           </div>
           {errorMessage && (
