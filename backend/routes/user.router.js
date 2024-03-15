a5c5f80e788d6cdb5f88eaa366e3176e3a18bf5f
farhan ansari <farhanraza2239@gmail.com>
2024-03-15 16:09:47 +0530

    get all users API created

--- backend/routes/user.router.js
+++ backend/routes/user.router.js
@@ -1,6 +1,6 @@
 
 import { Router } from "express";
-import { testApi,updateUser,deleteUser,logout } from "../controllers/user.controller.js";
+import { testApi,updateUser,deleteUser,logout,getUsers } from "../controllers/user.controller.js";
 import verifyToken from "../utility/userVerify.js";
 
 
@@ -16,6 +16,7 @@ router.get('/test',testApi)
 router.put('/update/:userId',verifyToken,updateUser)
 router.delete('/delete/:userId', verifyToken,deleteUser);
 router.post('/logout',logout);
+router.get('/getusers', verifyToken, getUsers);
 
 
 export default router
